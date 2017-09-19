860b2a799696cf44aceaab0af619e60311a3eaa0
johnkielar <johnkielar@wifi-roaming-128-4-60-129.host.udel.edu>
2017-09-19 13:46:52 -0400

    Using pkgEnum, constructed deck, might still need a second one and a method to draw.

--- FlamingoBLL/src/main/java/pkgCore/Deck.java
+++ FlamingoBLL/src/main/java/pkgCore/Deck.java
@@ -2,20 +2,32 @@ package pkgCore;
 
 import pkgEnum.eRank;
 import pkgEnum.eSuit;
-import java.util.ArrayList
+import java.util.ArrayList;
 
 public class Deck {
 
-	//	TODO: Add 'cards' attribute that is an ArrayList of Card
-	public static void main(String args[]) {
-		ArrayList<Card> list = new ArrayList<Card>();}
+public static void main(String args[]) {
+ArrayList<Card> list = new ArrayList<Card>();}
 		public int cards;
-
-	}
+		private ArrayList <Card> CardsInDeck;
+		public Deck ()
+		{
+			ArrayList<Card> cardDeck = new ArrayList<Card>();
+			
+			for (eSuit eSuit : eSuit.values()) {
+				
+				for (eRank eRank : eRank.values() )
+				{
+					Card c = new Card( eRank, eSuit);
+					cardDeck.add(c);
+				}
+			}
+			CardsInDeck = cardDeck;
+		}
+		}
 	
-	//	TODO: Add a contructor that passes in the number of decks, and then populates
-	//			ArrayList<Card> with cards (depending on number of decks).
 	
+	//	TODO: 
 	//			Example: Deck(1) will build one 52-card deck.  There are 52 different cards
 	//			2 clubs, 3 clubs... Ace clubs, 2 hearts, 3 hearts... Ace hearts, etc
 	
