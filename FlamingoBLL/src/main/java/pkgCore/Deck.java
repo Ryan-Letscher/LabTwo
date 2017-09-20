bd672118341776391bd62d4bc1581fc4866dd368
Ryan <Ryan@Ryan-Laptop>
2017-09-20 16:59:53 -0400

    yah

--- FlamingoBLL/src/main/java/pkgCore/Deck.java
+++ FlamingoBLL/src/main/java/pkgCore/Deck.java
@@ -3,37 +3,46 @@ package pkgCore;
 import pkgEnum.eRank;
 import pkgEnum.eSuit;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Deck {
 
-public static void main(String args[]) {
-ArrayList<Card> list = new ArrayList<Card>();}
-		public int cards;
-		private ArrayList <Card> CardsInDeck;
-		public Deck ()
-		{
-			ArrayList<Card> cardDeck = new ArrayList<Card>();
-			
-			for (eSuit eSuit : eSuit.values()) {
-				
-				for (eRank eRank : eRank.values() )
-				{
-					Card c = new Card( eRank, eSuit);
-					cardDeck.add(c);
-				}
+	//public static void main(String args[]) {
+	//	ArrayList<Card> list = new ArrayList<Card>();}
+	//public int cards;
+	private ArrayList <Card> CardsInDeck;
+	public Deck () {
+		for (eSuit eSuit : eSuit.values()) {
+			for (eRank eRank : eRank.values() ) {
+				Card c = new Card( eRank, eSuit);
+				CardsInDeck.add(c);
 			}
-			CardsInDeck = cardDeck;
 		}
-		}
-	
-	
-	//	TODO: 
-	//			Example: Deck(1) will build one 52-card deck.  There are 52 different cards
-	//			2 clubs, 3 clubs... Ace clubs, 2 hearts, 3 hearts... Ace hearts, etc
-	
-	//			Deck(2) will create an array of 104 cards.
-	
-	
-	//	TODO: Add a draw() method that will take a card from the deck and
-	//			return it to the caller
+		Collections.shuffle(CardsInDeck);
+	}
 
+	public Deck (int nums_of_deck) {
+		for(int x = 0; x < nums_of_deck; x++) {
+			
+		for (eSuit eSuit : eSuit.values()) {
+			for (eRank eRank : eRank.values() ) {
+				Card c = new Card( eRank, eSuit);
+				CardsInDeck.add(c);
+			}
+		}
+		}
+		Collections.shuffle(CardsInDeck);
+	}
+
+//	TODO: 
+//			Example: Deck(1) will build one 52-card deck.  There are 52 different cards
+//			2 clubs, 3 clubs... Ace clubs, 2 hearts, 3 hearts... Ace hearts, etc
+
+//			Deck(2) will create an array of 104 cards.
+
+
+
+//	TODO: Add a draw() method that will take a card from the deck and
+//			return it to the caller
+
+}
